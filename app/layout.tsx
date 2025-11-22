06060d947749b6862ea4342e6cbd2e4a4e0fb99e
Favour <favourson71@gmail.com>
2025-11-22 22:48:30 +0100

    fix: add globalloader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css';
 import { Navbar } from '@/app/components/Navbar';
 import Footer from './components/Footer';
+import ClientLoaderWrapper from './components/ClientLoaderWrapper';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,9 @@ export default function RootLayout({
         <link rel="icon" href="/BH.ico" sizes="any" />
         <div className="">
           <Navbar />
-          <main>{children}</main>
+          <ClientLoaderWrapper>
+            <main>{children}</main>
+          </ClientLoaderWrapper>
           <Footer />
         </div>
       </body>
