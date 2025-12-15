b1102454e5b7a39404c35a3407f9c484b9f87df4
Favour <favourson71@gmail.com>
2025-12-15 14:31:06 +0100

    feat: upgrade packages in package.json and add version number to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,10 @@ export default function Footer() {
   return (
     <footer className="w-full bg-white border-t mt-8">
       <div className="max-w-7xl mx-auto py-4 px-5 text-center text-sm text-gray-500">
-        &copy; {new Date().getFullYear()} BeHonest. All rights reserved. Version 0.2.1
+        &copy; {new Date().getFullYear()} BeHonest. All rights reserved.
+      </div>
+      <div>
+        V0.2.1
       </div>
     </footer>
   )
